a941dac746500b6bd607bd66304756787360faff
ahmed <arouibaa82@gmail.com>
2019-11-21 14:24:42 -0500

    created django app 1(movies)

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('create/', views.create, name='create'), # this mean calling  myapp.com/create. it will run create function when visit that link
+    path('edit/<str:movie_id>', views.edit, name='edit'), 
+    path('delete/<str:movie_id>', views.delete, name='delete'), 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import redirect    #needed for redirect to work in create function
+from django.contrib import messages      #needed for notification to work when create, delete, update function
 from airtable import Airtable
 import os
 
@@ -14,3 +15,54 @@ def home_page(request):
     search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
     stuff_for_frontend = {'search_result': search_result}
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+
+
+def create(request):            # is running when visit myapp.com/create
+    #print("haha")
+     if request.method == 'POST':   #POST should be capital
+         data ={
+             'Name': request.POST.get('name'),
+             "Pictures":[{
+                 "url": request.POST.get("url") or 'https://wingslax.com/wp-content/uploads/2017/12/no-image-available.png'}],   # if url is empty python use the url we gave
+             'Rating': int(request.POST.get('rating')),
+             'Notes': request.POST.get('notes')
+             
+             }
+         try:
+             response = AT.insert(data)   # airtable return a dictionary to us when inserting data
+             # notify on create
+             messages.success(request, 'New Movie added: {}'.format(response['fields'].get('Name'))) # .get('Name') is replacing ['Name'] so we don't get error when no Name exist
+         except Exception as e:
+             messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
+         
+
+     return redirect('/')   #take me to home page
+
+def edit(request, movie_id):
+     if request.method == 'POST':   #POST should be capital
+         data ={
+             'Name': request.POST.get('name'),
+             "Pictures":[{
+                 "url": request.POST.get("url") or 'https://wingslax.com/wp-content/uploads/2017/12/no-image-available.png'}],
+             'Rating': int(request.POST.get('rating')),
+             'Notes': request.POST.get('notes')       
+             }
+         try:
+                 response = AT.update(movie_id, data)
+                  # notify on edit
+                 messages.success(request, 'Updated movie: {}'.format(response['fields'].get('Name')))
+         except Exception as e:
+                messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
+
+     return redirect('/')
+
+def delete(request, movie_id):
+     try:
+        movie_name = AT.get(movie_id)['fields']['Name']
+    
+        response = AT.delete(movie_id)
+        # notify on delete
+        messages.warning(request, 'Movie deleted: {}'.format(movie_name))
+     except Exception as e:
+                messages.warning(request, 'Got an error when trying to delete a movie: {}'.format(e))
+     return redirect('/')
